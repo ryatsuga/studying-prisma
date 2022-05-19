9b208ee29960db7272be2d4044b882dc3ab18d6a
Leandro Passos <lf.passos11@gmail.com>
2022-05-19 13:28:40 -0300

    Fixes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-## Studying Prisma
+# Studying Prisma
 
-# Getting Start
+## Getting Start
+
 1. Run 'yarn' to install dependencies
 2. Run 'docker-compose -f k8s/stack.yml -p ignitenodejs up -d' to run a docker image from postgressql + adminer
-3. Run 'yarn prisma generate' to generate Models
-4. Write some code on server.ts file and run it with 'yarn ts-node-dev server.ts'
+3. Create a .env file with the content in the .env.example
+4. Run 'yarn prisma generate' to generate Models and 'yarn prisma migrate dev' to create database schema
+5. Write some code on server.ts file and run it with 'yarn ts-node-dev server.ts'
--- server.ts
+++ server.ts
@@ -1,20 +1,20 @@
 import { create } from './src/create';
 import { findOne } from './src/findOne';
-import { update } from './src/update';
+// import { update } from './src/update';
 
 async function main() {
 	// create({ name: 'ReactJS', duration: 250 });
 	// create({ name: 'Git', duration: 50 });
 	// findOne();
-	update({
-		id: '3e687d0a-2ffc-4e02-8e6d-f72d17ffc136',
-		name: 'ReactJS 2.0',
-		duration: 400,
-		description: 'Agora 2.0 com o mestre Diegão!',
-		teacher: {
-			name: 'João Lemos',
-		},
-	});
+	// update({
+	// 	id: '3e687d0a-2ffc-4e02-8e6d-f72d17ffc136',
+	// 	name: 'ReactJS 2.0',
+	// 	duration: 400,
+	// 	description: 'Agora 2.0 com o mestre Diegão!',
+	// 	teacher: {
+	// 		name: 'João Lemos',
+	// 	},
+	// });
 }
 
 main();
